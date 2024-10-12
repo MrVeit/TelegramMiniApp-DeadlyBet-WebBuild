9cab4ba74219bad7cc3749b5fc492f578ebcdc29
Mr_Veit <Misster.veit@gmail.com>
2024-10-13 03:16:55 +1200

    Minor update

---
+++ index.js
@@ -0,0 +1,117 @@
+document.addEventListener('DOMContentLoaded', function()
+{
+    if ('serviceWorker' in navigator)
+    {
+        var version = "1.0";
+        var filesToCache = [
+            './',
+            'index.html',
+            'TemplateData/Background.png',
+            'TemplateData/LoadCircle.png',
+            'TemplateData/favicon.ico',
+            'Build/8e864c2eaabbb39c726c2dc4cfa3daea.data.unityweb',
+            'Build/b6d7dc920e35302ed7c77a9e70035756.js.unityweb',
+            'Build/9b4d711547f668c3f568df1b4cfdce2e.wasm.unityweb',
+            'Build/TelegramMiniApp-DeadlyBet-WebBuild.loader.js'
+        ];
+    
+        navigator.serviceWorker.register('/ServiceWorker.js').then(function(registration)
+        {
+            console.log('Service Worker registered with scope:', registration.scope);
+    
+            if (registration.active)
+            {
+                registration.active.postMessage(
+                {
+                    title: 'SetVersion',
+                    version: version,
+                    files: filesToCache
+                });
+            }
+        }).catch(function(error)
+        {
+            console.log('Service Worker registration failed:', error);
+        });
+    }
+
+    var container = document.querySelector("#unity-container");
+    var canvas = document.querySelector("#unity-canvas");
+    var loadingBar = document.querySelector(".loader");
+
+    if (!canvas || !loadingBar) 
+    {
+        console.error("Canvas or loader element not found!");
+
+        return;
+    }
+
+    var buildUrl = "Build";
+    var loaderUrl = buildUrl + "/TelegramMiniApp-DeadlyBet-WebBuild.loader.js";
+    var config = {
+        dataUrl: buildUrl + "/8e864c2eaabbb39c726c2dc4cfa3daea.data.unityweb",
+        frameworkUrl: buildUrl + "/b6d7dc920e35302ed7c77a9e70035756.js.unityweb",
+        codeUrl: buildUrl + "/9b4d711547f668c3f568df1b4cfdce2e.wasm.unityweb",
+        streamingAssetsUrl: "StreamingAssets",
+        companyName: "BeniFun",
+        productName: "Deadly Bet",
+        productVersion: "1.0"
+    };
+
+    if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent))
+    {
+        var meta = document.createElement('meta');
+        meta.name = 'viewport';
+        meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
+        document.getElementsByTagName('head')[0].appendChild(meta);
+    }
+
+
+    loadingBar.style.display = "block";
+
+    var script = document.createElement("script");
+    script.src = loaderUrl;
+    
+    script.onload = () => 
+    {
+        createUnityInstance(canvas, config, (progress) =>
+        {
+
+        }).then((unityInstance) =>
+        {
+            unityInstanceRef = unityInstance;
+            loadingBar.style.display = "none";
+            console.log("Unity instance created successfully.");
+        }
+        ).catch((message) =>
+        {
+            console.error("Unity initialization error: " + message);
+        });
+    };
+
+    script.onerror = (error) =>
+    {
+        console.error("Failed to load Unity loader script:", error);
+    };
+    document.body.appendChild(script);
+});
+
+window.addEventListener('load', function ()
+{
+    Telegram.WebApp.ready();
+    Telegram.WebApp.expand();
+
+    console.log("Telegram web app has been expanded to full screen");
+
+    var version = Telegram.WebApp.version;
+    var versionFloat = parseFloat(version);
+
+    if (versionFloat >= 7.7)
+    {
+        Telegram.WebApp.disableVerticalSwipes();
+        console.log('Activating vertical swipe disable');
+    }
+
+    console.log(`Telegram Web App opened with version: ${version}`);
+    console.log(`Telegram Web App checked` +
+        `latest version status with result: ${Telegram.WebApp.isVersionAtLeast(version)}`);
+});
